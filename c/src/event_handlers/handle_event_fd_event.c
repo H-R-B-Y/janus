8fa15dd6cf5d6ebcc042b97e1b4532e80dc23bca
H-R-B-Y <hbreeze@student.42london.com>
2025-12-04 18:35:14 +0000

    testing new size and config

--- c/src/event_handlers/handle_event_fd_event.c
+++ c/src/event_handlers/handle_event_fd_event.c
@@ -6,7 +6,7 @@
 /*   By: hbreeze <hbreeze@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/04 16:03:59 by hbreeze           #+#    #+#             */
-/*   Updated: 2025/12/04 18:24:51 by hbreeze          ###   ########.fr       */
+/*   Updated: 2025/12/04 18:35:24 by hbreeze          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	handle_event_fd_event(struct s_janus_data *data, struct epoll_event *event)
 	ssize_t		s;
 #ifndef JANUS_TERMINAL_MODE
 	uint16_t	offset;
-	sFONT		*font_select = &Font16;
+	sFONT		*font_select = &Font20;
 #endif
 
 	s = read(event->data.fd, &event_count, sizeof(event_count));
@@ -33,7 +33,7 @@ int	handle_event_fd_event(struct s_janus_data *data, struct epoll_event *event)
 	if (data->interface_status == 0)
 	{
 #ifndef JANUS_TERMINAL_MODE
-		;
+		Paint_DrawString_EN(0, 10, "No network interfaces are up", font_select, BLACK, WHITE);
 #else
 		printf("Janus: No network interfaces are up\n");
 #endif
@@ -44,17 +44,25 @@ int	handle_event_fd_event(struct s_janus_data *data, struct epoll_event *event)
 		
 		if (is_interface_up(data->interface_status, JAN_ETH0))
 		{
+			// char eth0_str[64];
+			// snprintf(eth0_str, sizeof(eth0_str), "ETH0: %s", data->eth0_interface);
+			Paint_DrawString_EN(0, 10 + offset, "ETH0: Connected", font_select, BLACK, WHITE);
+			offset += font_select->Height + 1;
 			char eth0_str[64];
-			snprintf(eth0_str, sizeof(eth0_str), "ETH0: %s", data->eth0_interface);
+			snprintf(eth0_str, sizeof(eth0_str), "%s", data->eth0_interface);
 			Paint_DrawString_EN(0, 10 + offset, eth0_str, font_select, BLACK, WHITE);
-			offset += font_select->Height;
+			offset += font_select->Height + 1;
 		}
 		if (is_interface_up(data->interface_status, JAN_WLAN0))
 		{
+			// char wlan0_str[64];
+			// snprintf(wlan0_str, sizeof(wlan0_str), "WLAN0: %s", data->wlan0_interface);
+			Paint_DrawString_EN(0, 10 + offset, "WLAN0: Connected", font_select, BLACK, WHITE);
+			offset += font_select->Height + 1;
 			char wlan0_str[64];
-			snprintf(wlan0_str, sizeof(wlan0_str), "WLAN0: %s", data->wlan0_interface);
+			snprintf(wlan0_str, sizeof(wlan0_str), "%s", data->wlan0_interface);
 			Paint_DrawString_EN(0, 10 + offset, wlan0_str, font_select, BLACK, WHITE);
-			offset += font_select->Height;
+			offset += font_select->Height + 1;
 		}
 #else
 		printf("Janus: Network interfaces status:\n");
